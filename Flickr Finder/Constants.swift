68ba5439273435cd9b554d379c2879b282ead361
Mohamed Sayed <msayed@mobiquityinc.com>
2016-10-07 18:14:39 +0200

    Initialising FlickrKit framework

---
+++ Flickr Finder/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Flickr Finder
+//
+//  Created by Mohamed Sayed on 07/10/16.
+//  Copyright © 2016 Mohamed Sayed. All rights reserved.
+//
+
+import Foundation
+
+let kAPI_KEY    = "85bbb9eedc1a5a01b78c9deaffadebe0"
+let kSECRET     = "d6558cc5b278091f"
