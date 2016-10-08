52c101e76e882d2ecfc7808651d7fc612e0edb9b
Mohamed Sayed <msayed@mobiquityinc.com>
2016-10-08 21:47:59 +0200

    Photo model implementation

---
+++ Flickr Finder/PhotoModel.swift
@@ -0,0 +1,57 @@
+//
+//  PhotoModel.swift
+//  Flicker
+//
+//  Created by Mohamed Sayed on 05/10/16.
+//  Copyright © 2016 Mohamed Sayed. All rights reserved.
+//
+
+import Foundation
+
+class PhotoModel    : NSObject {
+    var photoUrl    : URL!
+    var title       : String!
+    var photoID     : String!
+    var owner       : String!
+    var location    : String!
+    var desc        : String!
+    
+    init(fromPhotoUrl passedPhotoUrl: URL,title passedTitle: String,owner ownerOfPhoto:String, andPhotoID phID:String){
+        photoUrl = passedPhotoUrl
+        title = passedTitle
+        photoID = phID
+        owner = ownerOfPhoto
+    }
+    
+    /**
+     * Returns all the available property values in the form of NSDictionary object where the key is the approperiate json key and the value is the value of the corresponding property
+     */
+    func toDictionary() -> NSDictionary
+    {
+        let dictionary = NSMutableDictionary()
+        if photoUrl != nil{
+            dictionary["photoUrl"] = photoUrl
+        }
+        if title != nil{
+            dictionary["title"] = title
+        }
+        
+        if photoID != nil{
+            dictionary["photoID"] = photoID
+        }
+        
+        if owner != nil{
+            dictionary["owner"] = photoID
+        }
+        
+        if location != nil{
+            dictionary["location"] = photoID
+        }
+        
+        if desc != nil{
+            dictionary["desc"] = photoID
+        }
+        return dictionary
+    }
+    
+}
